e2bf72830785754995caf5dd453ba36e447157ff
Justine adiarte <tatine.adiarte@gmail.com>
2025-04-25 00:46:39 +0800

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'pages/log_in_page.dart';
 import 'firebase_options.dart';
 import 'pages/home_page.dart';
 import 'pages/sign_up_page.dart';
-import 'pages/log_in_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
