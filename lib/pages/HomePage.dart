a1033c135046662772c9534f504935c653589c48
Jideeh1 <keonagjd@gmail.com>
2025-04-25 13:15:28 +0800

    Bottom Navigation Bar
    Added a bottom navigation bar with placeholder icons for yet-to-be-made assets

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import "package:flutter_svg/flutter_svg.dart";
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/main.dart';
+import "package:flutter_svg/flutter_svg.dart";
+// import 'main.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Hello, {name} !',
+          'test, {name} !',
           style: TextStyle(
             color: Color(0xFFFEFFFF),
             fontSize: 20,
@@ -19,6 +19,8 @@ class HomePage extends StatelessWidget {
           ),
         ),
         backgroundColor: Color(0xFF232834),
+        elevation: 15,
+        shadowColor: Colors.black,
         centerTitle: true,
         leading:
         // GestureDetector(
@@ -26,14 +28,14 @@ class HomePage extends StatelessWidget {
           // onTap: () {},
           margin: EdgeInsets.all(10),
           alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xFF424755),
+            borderRadius: BorderRadius.circular(100),
+          ),
           child: SvgPicture.asset(
             'assets/icons/user.svg',
             height: 30,
             width: 30,
-          ),
-          decoration: BoxDecoration(
-            color: Color(0xFF424755),
-            borderRadius: BorderRadius.circular(100),
           ),
         ),
         actions: [
@@ -43,14 +45,102 @@ class HomePage extends StatelessWidget {
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
             width: 37,
+            decoration: BoxDecoration(
+              color: Color(0xFF424755),
+              borderRadius: BorderRadius.circular(100),
+            ),
             child: SvgPicture.asset(
               'assets/icons/plus.svg',
               height: 20,
               width: 20,
             ),
-            decoration: BoxDecoration(
-              color: Color(0xFF424755),
-              borderRadius: BorderRadius.circular(100),
+          ),
+        ],
+      ),
+      // body: BottomTabs(),
+      bottomNavigationBar: BottomTabs(),
+    );
+  }
+}
+
+class BottomTabs extends StatefulWidget {
+  const BottomTabs({super.key});
+
+  @override
+  State<BottomTabs> createState() => _BottomTabsState();
+}
+
+class _BottomTabsState extends State<BottomTabs> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(
+            height: 100,
+            child: BottomNavigationBar(
+              showSelectedLabels: false,
+              showUnselectedLabels: true,
+              backgroundColor: Color(0xFF232834),
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      'assets/icons/placeholderIcon.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      'assets/icons/placeholderIcon.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      'assets/icons/placeholderIcon.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      'assets/icons/placeholderIcon.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      'assets/icons/user.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                  label: '',
+                ),
+              ],
             ),
           ),
         ],
